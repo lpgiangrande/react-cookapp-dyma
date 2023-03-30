d5f49b6ef3ef9e79e648104d16e54a1335f6a89f
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-03-30 11:07:30 +0200

    Designing Header and Footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
+import styles from "./Footer.module.scss";
+
 function Footer() {
-    return <div className="b3">Footer</div>
+  return (
+    <footer
+      className={`${styles.footer} d-flex flex-row align-items-center justify-content-center p-20`}
+    >
+      <p>Copyright © 2023 Cookchef D. Inc.</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,30 @@
+import styles from "./Header.module.scss";
+import logo from '../assets/images/cookchef.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/fontawesome-free-solid'; 
+import { faShoppingBasket } from '@fortawesome/fontawesome-free-solid'; 
+
+
 function Header() {
-    return <div className="b1">Header</div>
+    return (
+        <header className={`${styles.header} d-flex flex-row align-items-center`}>
+
+            <FontAwesomeIcon icon={faBars} />
+            <div className="flex-fill">
+                <img src={ logo } alt="logo cookchef"></img>
+            </div>
+            <ul>
+                <button className="mr-5 btn btn-reverse-primary">
+                    <FontAwesomeIcon className="mr-5" icon={faShoppingBasket} />
+                    panier
+                </button>
+                <button className="btn btn-primary">
+                    connexion
+                </button>
+            </ul>
+
+        </header>
+    );
 }
 
 export default Header;
