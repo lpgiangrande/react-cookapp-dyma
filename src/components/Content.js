bf8011e58073fec47eff816304e27b376e039066
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-05-08 21:14:06 +0200

    add recipes.js & data folder,  and map

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,19 @@
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
+import { data } from '../data/recipes';
 
 function Content() {
+
+  const recipes = data;
+
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={ styles.grid }>
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
+          { recipes.map((r => 
+            <Recipe title={r.title} image={r.image} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,16 +1,16 @@
 import styles from "./Recipe.module.scss";
-import recipe from "../assets/images/recette.jpg";
+// import recipe from "../assets/images/recette.jpg";
 
-function Recipe() {
+function Recipe({ title, image }) {
   return (
     <div className={styles.recipe}>
       <div className={styles.imageContainer}>
-        <img src={recipe} alt="recipe" />
+        <img src={image} alt="recipe" />
       </div>
       <div
         className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
       >
-        <h3>Saumon et asperges</h3>
+        <h3>{ title }</h3>
       </div>
     </div>
   );
