e93bd02955946d8d5f4c93795976f01e08db3812
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-07-12 14:31:27 +0200

    search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
-import styles from "./App.module.scss"
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className={ `d-flex flex-column ${styles.appContainer}`}>
-      <Header />  
-      <Content /> 
-      <Footer />   
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,40 @@
+import { useState } from "react";
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
 import { data } from "../data/recipes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Content() {
   const recipes = data;
+  const [filter, setFilter] = useState("");
+
+  function handleInput(e) {
+    const filter = e.target.value;
+    setFilter(filter.trim().toLowerCase());
+  }
 
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Nouvelles recettes</h1>
-      <div className={`card p-20 ${styles.contentCard}`}>
+      <div className={`card d-flex flex-column p-20 ${styles.contentCard}`}>
+        <div
+          className={`d-flex flew-row justify-content-center align-item-center my-30 ${styles.searchBar}`}
+        >
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="mr-15" />
+          <input
+            onInput={handleInput}
+            type="text"
+            className="flex-fill"
+            placeholder="Rechercher"
+          />
+        </div>
         <div className={styles.grid}>
-          {recipes.map((r) => (
-            <Recipe title={r.title} image={r.image} />
-          ))}
+          {recipes
+            .filter((r) => r.title.toLowerCase().startsWith(filter))
+            .map((r) => (
+              <Recipe key={r._id} title={r.title} image={r.image} />
+            ))}
         </div>
       </div>
     </div>
